1f9a8e9d752ff7170be014c76120b9e763a1ec74
Laura Incer <laura.incer@razorfish.com>
2018-01-22 20:32:38 -0600

    From review feedback: list number of comments per post in PostList and be able to edit/delete

--- src/modules/posts/components/PostList.js
+++ src/modules/posts/components/PostList.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { sortByScore, sortByDate, votePost } from './../PostActions'
+import { sortByScore, sortByDate, deletePost, votePost } from './../PostActions'
 
 class PostList extends Component {
+  removePost(postId) {
+    const { deletePost } = this.props;
+    deletePost(postId);
+  }
+
   votePost(postId, vote) {
     const { votePost } = this.props;
     votePost(postId, { option: vote });
@@ -25,9 +30,9 @@ class PostList extends Component {
             + Add New Post
           </Link>
           <div className="block">
-            <h4>Sort by:</h4>
-            <button className='btn btn--left' onClick={() => sortByScore()}>Score</button>
-            <button className='btn' onClick={() => sortByDate()}>Date</button>
+            Sort by:&nbsp;
+            <button className='btn btn--left btn--sort' onClick={() => sortByScore()}>Score</button>
+            <button className='btn btn--sort' onClick={() => sortByDate()}>Date</button>
           </div>
           <ul className='post-list'>
             {list && list.map((post) => (
@@ -45,11 +50,17 @@ class PostList extends Component {
                 <p className='category'>
                   Category: <span className='category-name'>{post.category}</span>
                 </p>
+                <button className='btn btn--left' onClick={() => this.removePost(post.id)}>Delete</button>
+                <Link className='btn btn--small' to={{
+                  pathname: '/edit',
+                  state: { 'post': post }
+                }}>Edit</Link>
                 <p className='block'>
                   Vote:&nbsp;
                   <button className='btn btn--left btn--vote' onClick={() => this.votePost(post.id, 'upVote')}>:)</button>
                   <button className='btn btn--vote' onClick={() => this.votePost(post.id, 'downVote')}>:(</button>
                 </p>
+                <p>Comments: <span className='number-comments'>{post.commentCount}</span></p>
               </li>
             ))}
           </ul>
@@ -68,6 +79,7 @@ function mapDispatchToProps(dispatch) {
   return {
     sortByScore: (data) => dispatch(sortByScore()),
     sortByDate: (data) => dispatch(sortByDate()),
+    deletePost: (data) => dispatch(deletePost(data)),
     votePost: (postId, data) => dispatch(votePost(postId, data))
   }
 }
