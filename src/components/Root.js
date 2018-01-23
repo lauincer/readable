da6ad30c6811b13c1167992a8cd3bcd6c794e103
Laura Incer <laura.incer@razorfish.com>
2018-01-22 21:48:27 -0600

    review feedback: implemented 404 page with Router <Switch>

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CategoryList from './../modules/categories/components/CategoryList'
 import PostList from './../modules/posts/components/PostList'
@@ -19,47 +19,58 @@ class Root extends Component {
   render() {
     const { category, posts } = this.props;
 
-    return (
-      <div>
-        <Route exact path="/" render={() => (
-          <div>
-            <CategoryList list={category.categoryList} />
-            <PostList list={posts.postList} />
-          </div>
-        )}/>
-        {category.categoryList && posts.postList && category.categoryList.map((cat) => (
-          <Route exact path={`/${cat.name}`}
-                 key={cat.name}
-                 render={() => (
-                  <div>
-                    <CategoryList list={category.categoryList} />
-                    <h2 className='category-title'>{cat.name}</h2>
-                    <PostList list={posts.postList.filter(post => (post.category === cat.name))}
-                              categoryName={cat.name} />
-                  </div>
-          )}/>
-        ))}
-        {posts.postList && posts.postList
-          .filter(post => (post.deleted === false))
-          .map(post => (
-            <Route exact path={`/${post.category}/${post.id}`}
-                   key={`post-page-${post.id}`}
-                   render={() => (
-                    <Post post={post} />
+    if (posts.postList && posts.postList) {
+      return (
+        <div>
+          <Switch>
+            <Route exact path="/" render={() => (
+              <div>
+                <CategoryList list={category.categoryList} />
+                <PostList list={posts.postList} />
+              </div>
             )}/>
-        ))}
-        <Route exact path='/create'
-               render={props => (
-                <PostCreate
-                  categoryName={props.location.state.categoryName} />
-        )}/>
-        <Route exact path='/edit'
-               render={props => (
-                <PostCreate
-                  post={props.location.state ? props.location.state.post : null} />
-        )}/>
-      </div>
-    );
+            {category.categoryList.map((cat) => (
+              <Route exact path={`/${cat.name}`}
+                     key={cat.name}
+                     render={() => (
+                      <div>
+                        <CategoryList list={category.categoryList} />
+                        <h2 className='category-title'>{cat.name}</h2>
+                        <PostList list={posts.postList.filter(post => (post.category === cat.name))}
+                                  categoryName={cat.name} />
+                      </div>
+              )}/>
+            ))}
+            {posts.postList
+              .filter(post => (post.deleted === false))
+              .map(post => (
+                <Route exact path={`/${post.category}/${post.id}`}
+                       key={`post-page-${post.id}`}
+                       render={() => (
+                        <Post post={post} />
+                )}/>
+            ))}
+            <Route exact path='/create'
+                   render={props => (
+                    <PostCreate
+                      categoryName={props.location.state.categoryName} />
+            )}/>
+            <Route exact path='/edit'
+                   render={props => (
+                    <PostCreate
+                      post={props.location.state ? props.location.state.post : null} />
+            )}/>
+            <Route render={() => (
+              <div className='content'>
+                <h1 className='not-found'>404 - Not Found</h1>
+              </div>
+            )}/>
+          </Switch>
+        </div>
+      );
+    }
+
+    return null;
   }
 }
 
