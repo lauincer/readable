a461e7bde5c29106bd3f032d0d854d43917c33bc
Laura Incer <laura.incer@razorfish.com>
2018-01-22 20:31:05 -0600

    Applied feedback suggestion: make App a Stateless functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 import './App.css';
 import Root from './Root'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Readable</h1>
-          <Link to='/'>Home</Link>
-        </header>
-        <Root/>
-      </div>
-    );
-  }
+export default function App () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Readable</h1>
+        <Link to='/'>Home</Link>
+      </header>
+      <Root/>
+    </div>
+  )
 }
-
-export default App;
